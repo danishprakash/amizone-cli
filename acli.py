5eed622249de084d8b81d8fdef00d2abd2c7df26
prakashdanish <grafitykoncept@gmail.com>
2017-07-28 15:50:54 +0530

    my faculty w/o subs

--- acli.py
+++ acli.py
@@ -2,7 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+
+def myFaculty(browser):
+    browser.find_element_by_xpath('/html/body/form/table/tbody/tr[2]/td[1]/table/tbody/tr[7]/td[2]/a').send_keys(Keys.RETURN)
+    time.sleep(5)
+    print('\nMy Faculty')
+    subs = browser.find_elements_by_xpath("//table[@id='tbl2']/tbody/tr")
+    #if i == 4 or i == 9:
+    #    continue
+    for i in range(1, len(subs)):
+        try:
+            print(browser.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td[3]/div/table[1]/tbody/tr[4]/td/div/table/tbody/tr["+str(i)+"]/td/table/tbody/tr/td/table/tbody/tr/td[2]/span[2]").text)
+        except:
+            pass
+    print(len(subs))
+
 def myCourses(browser):
+    browser.find_element_by_xpath('/html/body/form/table/tbody/tr[2]/td[1]/table/tbody/tr[6]/td[2]/a').send_keys(Keys.RETURN)
     core = browser.find_elements_by_xpath("//table[@id='ctl00_ContentPlaceHolder1_GridCourses']/tbody/tr")
     open_electives = browser.find_elements_by_xpath("//table[@id='ctl00_ContentPlaceHolder1_GridOpenCourses']/tbody/tr")
 
@@ -12,12 +28,12 @@ def myCourses(browser):
         print(str(i-1)+". " + browser.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td[3]/div/table/tbody/tr[3]/td/table/tbody/tr["+str(i) + "]/td[3]").text)
 
     for i in range(2, len(open_electives)+1):
-        print(str(i+len(core)-2)+". " +
-                browser.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td[3]/div/table/tbody/tr[4]/td/table/tbody/tr["+str(i)+ "]/td[3]").text)
-
+            print(str(i+len(core)-2)+". " +
+                    browser.find_element_by_xpath("/html/body/form/table/tbody/tr[2]/td[3]/div/table/tbody/tr[4]/td/table/tbody/tr["+str(i)+ "]/td[3]").text)
 
 def attendace(browser):
     print("\n Attendance:")
+    browser.find_element_by_xpath('/html/body/form/table/tbody/tr[2]/td[1]/table/tbody/tr[6]/td[2]/a').send_keys(Keys.RETURN)
     core = browser.find_elements_by_xpath("//table[@id='ctl00_ContentPlaceHolder1_GridCourses']/tbody/tr")
     open_electives = browser.find_elements_by_xpath("//table[@id='ctl00_ContentPlaceHolder1_GridOpenCourses']/tbody/tr")
     
@@ -36,7 +52,7 @@ def attendace(browser):
 
 
 def init():
-    browser = webdriver.Firefox()
+    browser = webdriver.Chrome()
     browser.get("https://amizone.net/amizone/")
     browser.set_window_size(1124, 850)
 
@@ -45,15 +61,17 @@ def init():
 
     username.send_keys("uid")
     password.send_keys("pwd")
+    time.sleep(5)
 
     browser.find_element_by_id('ImgBttn_Login').click()
+    time.sleep(5)
     browser.find_element_by_class_name('close').click()
+    time.sleep(5)
     print("Welcome, " + browser.find_element_by_id('ctl00_lblUser').text)
-    browser.find_element_by_xpath('/html/body/form/table/tbody/tr[2]/td[1]/table/tbody/tr[6]/td[2]/a').send_keys(Keys.RETURN)
     
-    time.sleep(3)
+    time.sleep(5)
     #myCourses(browser)
-    attendace(browser)
-    browser.close()
+    myFaculty(browser)
+    #browser.close()
 
 init()
